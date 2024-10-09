98b70fc14eb1db537f8ce3dacc39a57d6dfb2483
Matthew Morrone <matthewmorrone1@gmail.com>
2024-10-09 12:58:29 -0400

    modify path function

--- index.php
+++ index.php
@@ -14,23 +14,10 @@ function containsAnySubstring($string, $substrings) {
 }
 function query() {
     $qs = []; 
-    [$page, $name] = array_slice(explode("/", $_SERVER['REQUEST_URI']), 1); 
+    [$root, $page, $name] = explode("/", $_SERVER['REQUEST_URI']); 
     $qs["page"] = $page; 
     $qs["name"] = $name;
     return $qs;
-}
-function request_path() {
-    $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-    $script_name = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
-    $parts = array_diff_assoc($request_uri, $script_name);
-    if (empty($parts)) {
-        return '/';
-    }
-    $path = implode('/', $parts);
-    if (($position = strpos($path, '?')) !== FALSE) {
-        $path = substr($path, 0, $position);
-    }
-    return $path;
 }
 ?>
 <!DOCTYPE html>
