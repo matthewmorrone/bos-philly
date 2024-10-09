406a6c2a51af9303931311136eac2924914905d4
Matthew Morrone <matthewmorrone1@gmail.com>
2024-10-09 12:55:19 -0400

    turn off error reporting

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 include ("wordpress/wp-config.php");
 function isMobile() {
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
