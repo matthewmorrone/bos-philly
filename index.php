ef7b26b71c888f0f94706b9a15e1d16b00e65814
Matthew Morrone <matthewmorrone1@gmail.com>
2024-10-24 15:41:02 -0400

    add version to css link tag

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ function query() {
 <meta name="viewport" content="width=device-width, initial-scale=.9, viewport-fit=cover">
 <base href="/" />
 <title>BOS Philly - Bringing Circuit Back to Philly</title>
-<link rel="stylesheet" href="css/index.css" />
+<link rel="stylesheet" href="css/index.css?version=1" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
