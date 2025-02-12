52ff7fd636aa988c2f625b419c1edcdc738eda49
Matthew Morrone <matthewmorrone1@gmail.com>
2025-02-11 21:00:20 -0500

    fix mobile bar

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'bos' );
+define( 'DB_NAME', 'db' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'db' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'mysql' );
+define( 'DB_PASSWORD', 'db' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', 'db' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
