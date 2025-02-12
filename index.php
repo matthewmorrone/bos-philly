545b7c46da5a91017c31331da1a1e700a5879255
Matthew Morrone <matthewmorrone1@gmail.com>
2025-02-12 06:54:09 -0500

    fix links

--- index.php
+++ index.php
@@ -433,7 +433,7 @@ apply_filters('the_content', get_post_field('post_content', $page->id));
     <div>
         <h2>Committed To Philadelphia</h2>
         <h3>BOS Philly, a 501c3, brings circuit parties to Philadelphia with international DJs and top-notch production while supporting local LGBT charities. Our events are unique and inclusive, creating an unforgettable experience. Join us to make a positive impact on the community while dancing the night away.</h3>
-        <p><a href="https://www.bosphilly.org/board/">Learn more about us >></a></p>
+        <p><a href="about-us">Learn more about us >></a></p>
     </div>
     <style>
     #pandering {
@@ -615,7 +615,7 @@ endforeach;
         </div>
     </div>
     <div class="button-container">
-        <button class="more" href="wordpress/about-us">About Us...</button>
+        <button class="more" href="about-us">About Us...</button>
     </div>
 </section>
 <?php endif; ?>
