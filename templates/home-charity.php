258d040ee954944f03c798b4ddfea95ebb39e9c5
Matthew Morrone <matthewmorrone1@gmail.com>
2025-09-12 21:44:28 -0400

    update charity

--- templates/home-charity.php
+++ templates/home-charity.php
@@ -3,8 +3,8 @@
         <div id='parallax'></div>
     </div>
     <div class="charity-title">
-        <h2><span class="counter" start="0.00" current="0.00" end="77497.33"></span></h2>
-        <!-- last updated 2024/10/29 -->
+        <h2><span class="counter" start="0.00" current="0.00" end="82497.33"></span></h2>
+        <!-- last updated 2025/09/12 -->
         <p class="counter-title">Donated to local LGBT Charities since 2018</p>
     </div>
 </section>
