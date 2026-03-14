c8d2d361dce0b1957d6725ffe86437e1cca5edc0
Matthew Morrone <matthewmorrone1@gmail.com>
2026-03-14 15:53:25 -0400

    feat: add Google Tag Manager and Meta Pixel tracking scripts to header.php

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,27 @@
 <?php $media_base = function_exists('bos_media_prod_base_url') ? bos_media_prod_base_url() : 'https://www.bosphilly.org'; ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TNNFCZT4');</script>
+<!-- End Google Tag Manager -->
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1255364439052457');
+fbq('track', 'PageView');
+</script>
+<!-- End Meta Pixel Code -->
 <base href="/" />
 <title>BOS Philly - Bringing Circuit Back to Philly</title>
 <link rel="stylesheet" href="css/index.css?version=<?= asset_version('css/index.css'); ?>" />
@@ -41,6 +62,15 @@ gtag('config', 'AW-17640299113');     // Google Ads
 <!-- End Facebook verification -->
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNNFCZT4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!-- Meta Pixel Code (noscript) -->
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1255364439052457&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code (noscript) -->
 <header class="fixed">
     <div class="header-inner">
         <a href="/" id="home">
